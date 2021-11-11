3e87167c9172de36a3313eca8ff6cf623c4df0b1
Owner <alsalamcse>
2021-11-11 08:55:16 +0200

    finished alert dialog

--- app/src/main/java/ranii/awad/ranitasksmanger/SplashScreenActivity.java
+++ app/src/main/java/ranii/awad/ranitasksmanger/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package ranii.awad.ranitasksmanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,5 +15,20 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         imgview=findViewById(R.id.imgView);
+        Thread th=new Thread()
+        {
+            //thread 3
+            public void run()
+            {
+                int i=3*1000;
+                try{
+                    sleep(i);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        th.start();
     }
 }
